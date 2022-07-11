0d87e23663acc8e4ddadbbffec1248090db0e5a3
aidanliddiard <aidanliddiard@gmail.com>
2022-07-11 11:14:41 -0700

    adding user test passing

---
+++ __tests__/users.test.js
@@ -0,0 +1,27 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+const mockUser = {
+  email: 'test@testing.com',
+  password: '123456',
+};
+
+describe('user backend routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('creates a new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+    const { email } = mockUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email,
+    });
+  });
+  afterAll(() => {
+    pool.end();
+  });
+});
